97a8288835b1f60ef80832470815e5e567335bbb
Nicole Ocholla <nicoleochola@gmail.com>
2019-10-27 22:47:49 +0300

    Modified date calculation function to give accurate results

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,24 @@
-var day = parseInt(document.getElementById ("day").value);
-var month = parseInt(document.getElementById ("month").value);
-var year = parseInt((document.getElementById ("year").value).substring(2, 3));
-var gender = document.getElementById ("gender").value;
-var century = parseInt((year/100) + 1);
-
-function getDayIndex(day, month, year, century)
+function displayAkanName()
 {
-  return Math.round(parseInt(((century/4) - 2 * century - 1) + ((5*year/4)) + ((26 * (month+1)/10)) + day ) % 7);
+  var gender = document.querySelector('input[name="Gender"]:checked').value;
+  var dayIndex = getDayIndex(year, month, day);
+
+  alert("Your Akan name is: " + getAkanName(dayIndex, gender));
 }
 
-// ((20/4) - ((2 * 20) - 1)) + (((5 * 96)/4) ) + ((((26 *(3 + 1))/10) ) + 30 ) % 7;
+function getDayIndex()
+{
+  var day = parseInt(document.getElementById ("day").value);
+  var month = parseInt(document.getElementById ("month").value);
+  var year = parseInt(document.getElementById ("year").value);
 
-// ( ( (CC/4) - (2 * CC - 1)) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+  return new Date(year, month - 1, day).getDay();
+}
 
-// ( ( (20/4) - 2 * 20 - 1) + ((5*96/4) ) + ((26*(03+1)/10)) + 30 ) % 7;
-
-// ((20/4) - (2 * 20 - 1)) + (5 * 96/4) + ((26*(3 + 1)/10)) + 30 ) % 7;
-
-function akanName(dayIndex, gender)
+function getAkanName(dayIndex, gender)
 {
   var boyNames = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var girlNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   
-  return gender == "Male" ? boyNames[dayIndex] : girlNames[dayIndex];
+  return gender === "Male" ? boyNames[dayIndex] : girlNames[dayIndex];
 }
-
-function getGender()
-{
-  return document.getElementById("gender").value == "Male" ? "Male" : "Female";
-}
-
