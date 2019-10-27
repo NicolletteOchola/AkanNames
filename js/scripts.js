312fa2ab2a56386cacb7f9f954dd415543590a08
Nicole Ocholla <nicoleochola@gmail.com>
2019-10-27 09:38:33 +0300

    Declared the variables day, month and year

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 function akanName {
-  
+  var day = document.getElementById ("day").value;
+  var month = document.getElementById ("month").value;
+  var year = document.getElementById ("year").value;
+ 
 }
