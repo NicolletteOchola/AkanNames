f53219145178b21d6b1810bd962a28ca37bf64d9
Nicole Ocholla <nicoleochola@gmail.com>
2019-10-28 15:08:02 +0300

    Added a function to display invalid when erreneous months and dates were put in

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 function displayAkanName()
 {
-  var gender = document.querySelector('input[name="Gender"]:checked').value;
-  var dayIndex = getDayIndex(year, month, day);
-
-  alert("Your Akan name is: " + getAkanName(dayIndex, gender));
-}
-
-function getDayIndex()
-{
+  
   var day = parseInt(document.getElementById ("day").value);
   var month = parseInt(document.getElementById ("month").value);
   var year = parseInt(document.getElementById ("year").value);
+  var gender = document.querySelector('input[name="Gender"]:checked').value;
+  if(invlid(day,month)){
+  var dayIndex = new Date(year, month - 1, day).getDay();
 
-  return new Date(year, month - 1, day).getDay();
+  alert("Your Akan name is: " + getAkanName(dayIndex, gender));
+  }
 }
+
+// function getDayIndex()
+// {
+//   var day = parseInt(document.getElementById ("day").value);
+//   var month = parseInt(document.getElementById ("month").value);
+//   var year = parseInt(document.getElementById ("year").value);
+
+//   return new Date(year, month - 1, day).getDay();
+// }
 
 function getAkanName(dayIndex, gender)
 {
@@ -21,4 +27,17 @@ function getAkanName(dayIndex, gender)
   var girlNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   
   return gender === "Male" ? boyNames[dayIndex] : girlNames[dayIndex];
+}
+
+function invlid(day, month)
+{
+  if ( month < 1 || month > 12 || month == 2 && day > 29 ) {
+    alert("Month entered is Invalid")  ;
+    return false;
+  }else if ( day < 1 || day > 31) {
+    return false;
+    alert("Day entered is invalid");
+  }else{
+    return true;
+  }
 }
